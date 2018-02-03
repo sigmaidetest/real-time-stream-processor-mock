09928b5c691ef1b0047d451a26be7c7c092bd1d3
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-02-04 02:12:48 +0530

    Added licence header and formatted the process and persist record lambda file

--- real-time-stream-processor-mock/process-and-persist-record.js
+++ real-time-stream-processor-mock/process-and-persist-record.js
@@ -1,11 +1,25 @@
-let AWS = require('aws-sdk');
+/*
+ * SLAppForge Sigma. Integrated Development Environment for Serverless Computing
+ *
+ * Copyright (c) 2018 SLAppForge Lanka Private Limited. All Rights Reserved.
+ * https://www.slappforge.com
+ *
+ * You may use this file in accordance with the License Agreement provided with the Software or, alternatively, 
+ * in accordance with the terms contained in a written agreement between you and SLAppForge.
+ *
+ * If you have questions regarding the use of this file, please contact SLAppForge at info@slappforge.com
+ */
+
+const AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
+
 exports.handler = function (event, context, callback) {
 
 	event.Records.forEach(record => {
 
 		let payload = JSON.parse(new Buffer(record.kinesis.data, 'base64').toString('ascii'));
 
+		// Partition key and sort key should be non null values
 		if (payload.ip !== null && payload.datetime !== null) {
 
 			ddb.put({
